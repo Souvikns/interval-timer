7a300489d3faf4cf69cb74cc88badebb0b9e0288
Souvik <souvikde.ns@gmail.com>
2020-12-25 01:33:39 +0530

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,32 @@
 const { app, BrowserWindow, Tray } = require('electron');
+const path = require('path');
 
 let tray = null;
 let window = null;
 app.dock.hide();
+
+
+const createTray = () => {
+    tray = new Tray(path.join(__dirname, 'assets/time.png'));
+    tray.addListener('click', createWindow);
+}
+
+const createWindow = () => {
+    window = new BrowserWindow({
+        width: 600,
+        height: 450,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    window.loadFile(path.join(__dirname, 'screens/index.html'))
+}
+
+app.whenReady().then(createTray);
+
+
+app.on('window-all-closed', () => {
+    if(process.platform !== "darwin"){
+        app.quit()
+    }
+})
