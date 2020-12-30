8f0b3d8a863757e7d76a81891c82b62b64aad936
Souvik <souvikde.ns@gmail.com>
2020-12-30 22:53:19 +0530

    Update timer.js

--- src/mainscreen/timer.js
+++ src/mainscreen/timer.js
@@ -13,7 +13,14 @@ class Timer {
 
     start(time, interval, cb) {
         this.interval = interval;
-        
+        setInterval(() => {
+            /**
+             * If the diff between the current time and the 
+             * start time is equal to interval we call the callback,
+             * and update the start time with the current time. 
+             */
+            
+        }, 1000)
     }
 
 }
