8d0fa546d99f9824d07aa2bb2f479dec26b866ea
Souvik <souvikde.ns@gmail.com>
2021-01-13 15:34:37 +0530

    Update render.js

--- src/mainscreen/render.js
+++ src/mainscreen/render.js
@@ -1,11 +1,26 @@
 const { ipcRenderer } = require('electron');
 const { Timer } = require('./timer');
 
-document.getElementById("closebtn").addEventListener('click', () => {
-    ipcRenderer.invoke('hide-window');
-})
+// document.getElementById("closebtn").addEventListener('click', () => {
+//     ipcRenderer.invoke('hide-window');
+// })
 
 document.getElementById("start-timer").addEventListener('click', () => {
     
     
 })
+
+class App{
+    constructor(){
+        this.closeBtn = document.getElementById('closebtn');
+    }
+    run(){
+        this.closeBtn.addEventListener('click',() => {
+            ipcRenderer.invoke('hide-window');
+        })
+    }
+}
+
+let app = new App();
+
+app.run();
