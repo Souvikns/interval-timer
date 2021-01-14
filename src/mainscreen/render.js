4b6e6b896ac6fe9d121c6274881ac245925a3e32
Souvik <souvikde.ns@gmail.com>
2021-01-14 22:08:21 +0530

    Update render.js

--- src/mainscreen/render.js
+++ src/mainscreen/render.js
@@ -1,13 +1,20 @@
 const { ipcRenderer } = require('electron');
 const { Timer } = require('./timer');
 
+
+//constants
+const closebtn = document.getElementById('closebtn');
+const mainScreen = document.getElementById('mainscreen')
+const secondScreen = document.getElementById('secondscreen');
+
+
 document.getElementById("closebtn").addEventListener('click', () => {
     ipcRenderer.invoke('hide-window');
 })
 
 document.getElementById("start-timer").addEventListener('click', () => {
-    document.getElementById('mainscreen').hidden = true;
-    document.getElementById('secondscreen').hidden = false;
+    mainScreen.hidden = true;
+    secondScreen.hidden = false;
 })
 
-document.getElementById('secondscreen').hidden = true
+secondScreen.hidden = true;
