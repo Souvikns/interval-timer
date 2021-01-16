c318a04fa797ccf25f79845823d5437330bc849c
Souvik <souvikde.ns@gmail.com>
2021-01-16 18:43:43 +0530

    Update render.js

--- src/mainscreen/render.js
+++ src/mainscreen/render.js
@@ -2,9 +2,9 @@ const { ipcRenderer } = require('electron');
 const { Timer } = require('./timer');
 
 // // preventing window from reloading
-// window.addEventListener('beforeunload', (ev) => {
-//     ev.returnValue = true;
-// })
+window.addEventListener('beforeunload', (ev) => {
+    ev.returnValue = true;
+})
 
 
 //constants
