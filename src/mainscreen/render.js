aa58591d04e9ce5d0d1807e664b1eb2607590d62
Souvik <souvikde.ns@gmail.com>
2021-01-17 15:54:58 +0530

    start function in the timer class is working

--- src/mainscreen/render.js
+++ src/mainscreen/render.js
@@ -12,6 +12,18 @@ const closebtn = document.getElementById('closebtn');
 const mainScreen = document.getElementById('mainscreen')
 const secondScreen = document.getElementById('secondscreen');
 const startTimerButton = document.getElementById('start-timer');
+const stopTimerButton = document.getElementById('stop-timer');
+const timeSelect = document.getElementById('time-select');
+
+const timeInput = document.getElementById('time-input');
+
+const getTimeInput = () => {
+    return parseInt(timeInput.value);
+}
+
+const getTimeSelect = () => {
+    return timeSelect.value;
+}
 
 closebtn.addEventListener('click', () => {
     ipcRenderer.invoke('hide-window');
@@ -20,4 +32,11 @@ closebtn.addEventListener('click', () => {
 startTimerButton.addEventListener('click', () => {
     mainScreen.style.display = "none";
     secondScreen.style.display = "block";
+    let timer = new Timer();
+    timer.start({ val: getTimeInput(), time: getTimeSelect() }, () => {
+        let notification = new Notification("Drink Water", { "vibrate": true })
+    }, tick => {
+
+    })
 });
+
